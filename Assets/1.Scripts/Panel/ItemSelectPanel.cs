b7d8d2e5e062a83b1a873008a04887b65de255aa
이수현 <dltngus7535@gmail.com>
2026-04-24 02:43:11 +0900

    아이템 선택 스크립트, 씬작업, 폴더정리

--- Assets/1.Scripts/Panel/ItemSelectPanel.cs
+++ Assets/1.Scripts/Panel/ItemSelectPanel.cs
@@ -1,16 +1,67 @@
+using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class ItemSelectPanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private CanvasGroup cg;
+    [SerializeField] private GameObject itemPrefab;
+    [SerializeField, ReadOnly] private Toggle[] toggles;
+    [SerializeField] private Button okBtn;
+    private const int SelectMaxCount = 3;
+    private int _currentSelectCount = 0;
+    
+    private void Start()
     {
-        
+        SetToggleEvent();
+        SetButtonEvent();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActiveCg(bool isActive) => cg.ActiveCG(isActive);
+
+    private void SetButtonEvent()
     {
-        
+        okBtn.onClick.AddListener(() =>
+        {
+            ActiveCg(false);
+        });
+    }
+    
+    private void SetToggleEvent()
+    {
+        foreach (var toggle in toggles)
+        {
+            toggle.onValueChanged.AddListener((isOn) =>
+            {
+                if (isOn)
+                    ++_currentSelectCount;
+                else
+                    --_currentSelectCount;
+                if (_currentSelectCount > SelectMaxCount)
+                {
+                    toggle.isOn = false;
+                    return;
+                }
+                ActiveInteractable(_currentSelectCount != SelectMaxCount);
+            });
+        }
+
+        void ActiveInteractable(bool active)
+        {
+            if (active)
+            {
+                foreach (var toggle in toggles)
+                    toggle.interactable = true;
+                okBtn.interactable = false;
+            }
+            else
+            {
+                foreach (var toggle in toggles)
+                    toggle.interactable = toggle.isOn;
+                okBtn.interactable = true;
+            }
+        }
     }
 }
