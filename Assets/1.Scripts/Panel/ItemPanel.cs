9b516cfb7058275f43f857221b187657b27646f5
slswkdkqkdl-hue <slswkdkqkdl@gmail.com>
2026-05-06 02:07:02 +0900

    토글 초기화

--- Assets/1.Scripts/Panel/ItemPanel.cs
+++ Assets/1.Scripts/Panel/ItemPanel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -8,6 +9,7 @@ public class ItemPanel : MonoBehaviour
     [SerializeField] private ItemToggle _itemPrefab;
     [SerializeField] private ToggleGroup _toggleGroup;
     [SerializeField] private Transform bg;
+    private List<Toggle> _toggles = new List<Toggle>();
     public void Set(GomokuItem[] items)
     {
         foreach (var item in items)
@@ -15,6 +17,7 @@ public class ItemPanel : MonoBehaviour
             var itemObj = Instantiate(_itemPrefab, bg);
             itemObj.Set(item);
             itemObj.toggle.group = _toggleGroup;
+            _toggles.Add(itemObj.toggle);
             itemObj.toggle.onValueChanged.AddListener((isOn) =>
             {
                 if (isOn)
@@ -29,6 +32,14 @@ public class ItemPanel : MonoBehaviour
                 }
                 
             });
+        }
+    }
+    // 턴이 바뀔 때 호출할 함수
+    public void ClearAllToggles()
+    {
+        foreach (var t in _toggles)
+        {
+            t.isOn = false; 
         }
     }
 }
